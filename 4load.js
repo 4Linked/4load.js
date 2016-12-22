7897e53606bb02e9e668f426bbccfd5abd12b579
faustoct <fausto.alemao@gmail.com>
2016-12-22 06:17:25 -0200

    Update 4load.js

--- 4load.js
+++ 4load.js
@@ -2,13 +2,10 @@
 
 
 var cs = document.currentScript;
-console.log(cs)
 var modules = cs.getAttribute('data-modules')
 var modules = JSON.parse(JSON.stringify(eval("(" + modules + ")")))
-
 for(var i=0;i<modules.length;i++){
   var tag = document.createElement('script');
-  console.log(modules[i])
   tag.src = modules[i]['src']
   tag.async= modules[i]['async'] ? false : true
   document.body.appendChild(tag);
